83941212b1c8fa76353d5acaac6e5347277acdb1
Seammerman <seammerman@gmail.com>
2017-02-23 16:49:47 -0500

    nothing different

--- MatrixHead.h
+++ MatrixHead.h
@@ -1,41 +1,89 @@
 /*
-header file to hold the matrix class for use in later projects as well as the current project 1
-template found at (https://isocpp.org/wiki/faq/operator-overloading#matrix-subscript-op)
 Currently has no way to handle incorrect shapes
 Must be used to define square matrices (M x M)
+
+header file to hold the matrix class for use in later projects as well as the current project 1
+template found at (https://isocpp.org/wiki/faq/operator-overloading#matrix-subscript-op)
+
 */
+
 class Matrix
 {
 public:
 	Matrix(unsigned row, unsigned col);
 	double& operator() (unsigned row, unsigned col);
-	double operator()(unsigned row, unsigned col) const;
+	double operator() (unsigned row, unsigned col) const;
 	//..
 	~Matrix(); // Destructor
 	Matrix(const Matrix& m); // copy constructor
 	Matrix& operator= (const Matrix& m); // Assignment operator
-										 // ...
+	
+									 // ...
 private:
 	unsigned rows_, cols_;
 	double* data_;
 };
 
-inline
-Matrix::Matrix(unsigned rows, unsigned cols) :rows_(rows), cols_(cols)
+
+Matrix::Matrix(unsigned rows, unsigned cols) 
+	:rows_(rows)
+	,cols_(cols)
 {
 	data_ = new double[rows * cols];
 }
-inline
+
 Matrix::~Matrix() {
 	delete[] data_;
 }
-inline
-double& Matrix:: operator()(unsigned row, unsigned col)
+
+double& Matrix::operator()(unsigned row, unsigned col)
 {
-	return data_[cols_*row + col];
+	return data_[row*cols_ + col];
 }
-inline
+
 double Matrix::operator()(unsigned row, unsigned col) const
 {
-	return data_[cols_*row + col];
+	return data_[row*cols_ + col];
 }
+
+
+/*
+class Matrix
+{
+public:
+	Matrix(size_t rows, size_t cols);
+	double& operator()(size_t i, size_t j);
+	double operator()(size_t i, size_t j) const;
+	void print()const;
+private:
+	size_t mRows;
+	size_t mCols;
+	std::vector<double> mData;
+};
+
+Matrix::Matrix(size_t rows, size_t cols)
+	: mRows(rows),
+	mCols(cols),
+	mData(rows * cols)
+{
+}
+
+double& Matrix::operator()(size_t i, size_t j)
+{
+	return mData[i * mCols + j];
+}
+
+double Matrix::operator()(size_t i, size_t j) const
+{
+	return mData[i * mCols + j];
+}
+void Matrix::print()const {
+	for (int j = 0; j < mRows; j++) {
+		for (int k = 0; k < mCols; k++) {
+			std::cout << j <<" , "<< k << "  ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+*/
